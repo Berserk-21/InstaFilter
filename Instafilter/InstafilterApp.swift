4c8d4d4037beae451bf3310b851c38bc3ef6e838
Berserk-21 <cryptogekkofr@protonmail.com>
2024-06-24 16:28:47 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Berserk on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
