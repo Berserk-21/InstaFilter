a8b2f8ab2fb42157e115c4ba66b850f49aeac775
Berserk-21 <cryptogekkofr@protonmail.com>
2024-06-25 09:26:50 +0200

    Create a basic UI to import and modify photo

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        AppStoreReviewView()
+        
+        NavigationStack {
+            
+            VStack {
+                Spacer()
+                
+                if let image = processedImage {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+        }
+    }
+    
+    func changeFilter() {
+        
+        
     }
 }
 
---
+++ Instafilter/ModifyImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ModifyImageView.swift
+//  Instafilter
+//
+//  Created by Berserk on 25/06/2024.
+//
+
+import SwiftUI
+
+struct ModifyImageView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ModifyImageView()
+}
